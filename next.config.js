e17b9b9c4b0db0c35bfbbc0c4db24e71423e3c78
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-01-24 07:53:06 -0500

    feat: add X-Frame-Options security header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const securityHeaders = [{
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN'
+}
+]
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply the security headers to all routes in the application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
