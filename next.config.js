01ec73e59d4db1eb9a95ffa217aadd20328b5c59
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-01-25 06:55:37 -0500

    feat: add security header Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const securityHeaders = [
   {
     key: 'Permissions-Policy',
     value: 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'none'; connect-src 'self' https://vitals.vercel-insights.com/v1/vitals; img-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self'",
   }
 ]
 
