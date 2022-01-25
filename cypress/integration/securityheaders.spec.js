5262a800de78822823200f3257aa3715a54bb9ed
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-01-25 07:42:46 -0500

    test: add tests for additional security headers

--- cypress/integration/securityheaders.spec.js
+++ cypress/integration/securityheaders.spec.js
@@ -7,6 +7,12 @@ describe("Navigation", () => {
       expect(response.headers, "response headers").to.include({
         "x-frame-options": "SAMEORIGIN",
         "x-content-type-options": "nosniff",
+        "referrer-policy": "no-referrer-when-downgrade",
+        "permissions-policy":
+          "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()",
+        "content-security-policy":
+          "default-src 'none'; connect-src 'self' https://vitals.vercel-insights.com/v1/vitals; img-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self'",
+        "x-xss-protection": "1; mode=block",
       });
     });
   });
