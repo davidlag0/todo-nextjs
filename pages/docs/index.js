b704a4f9e1786e05b08ed3ac44b42f0413f07d08
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-08-23 00:04:29 -0400

    chore: convert OpenAPI spec to JSON

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,6 +1,6 @@
 // TODO: CSP policy needs to be adjusted because of the below.
 import { RedocStandalone } from "redoc";
-import openapispec from "../../public/openapi.yml";
+import openapispec from "../../public/openapi.json";
 
 export default function Docs() {
   return <RedocStandalone spec={openapispec} />;
