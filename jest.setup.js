5d38ac77c4fe51007cb8d669387a546f8c417808
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-12-19 06:36:44 -0500

    test: resolve failures due to the test environment

--- jest.setup.js
+++ jest.setup.js
@@ -11,6 +11,8 @@ import { TextEncoder, TextDecoder } from "util";
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-import fetchMock from "jest-fetch-mock";
-fetchMock.enableMocks();
-fetchMock.dontMock();
+if (typeof global.fetch === "undefined") {
+  const fetchMock = require("jest-fetch-mock");
+  fetchMock.enableMocks();
+  fetchMock.dontMock();
+}
