874bf84181dfb2bdf00818f5faa0e8b67b513ab8
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-12-18 21:32:47 -0500

    feat(redoc): remove along with deps
    This is to avoid staying too much behind for new Next.js and React
    features. React 18 has been out for a while and still no support for it
    in redoc.

--- pages/docs/index.js
+++
@@ -1,7 +0,0 @@
-// TODO: CSP policy needs to be adjusted because of the below.
-import { RedocStandalone } from "redoc";
-import openapispec from "../../public/openapi.json";
-
-export default function Docs() {
-  return <RedocStandalone spec={openapispec} />;
-}
