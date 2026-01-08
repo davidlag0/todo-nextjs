df2a71384f5c2c03958f671acc7ff271b244f018
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2026-01-08 06:05:03 -0500

    chore: update nextjs to v16.1.1

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "coverage/**",
+  ]),
+]);
+
+export default eslintConfig;
