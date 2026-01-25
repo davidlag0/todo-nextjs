e0f81faac45237234f5370af530aa85704e8ed4c
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2026-01-24 19:28:35 -0500

    chore: upgrade prisma to v7

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
   migrations: {
     seed: "tsx prisma/seed.ts",
   },
